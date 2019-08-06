b02163bb28e110fac6cfb562cb845ba98701ace2
Israel Ortiz <ortiz296@csusm.edu>
2019-08-06 15:29:41 -0700

    day2-part2

--- main.py
+++ main.py
@@ -57,20 +57,11 @@ class Game(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', SpeedClicker),
     ('/shooting-range', Game)
->>>>>>> c0ad6c49248c00a4c219bff8b7c34af4aad57bfc
 
 ], debug=True)
 
-class Timer:
-  def __init__(self):
-    self.start = time.time()
-
-  def restart(self):
-    self.start = time.time()
-
-  def get_time_hhmmss(self):
-    end = time.time()
-    m, s = divmod(end - self.start, 60)
-    h, m = divmod(m, 60)
-    time_str = "%02d:%02d:%02d" % (h, m, s)
-    return time_str
+for i in range(8)
+    m = Mob()
+    all_sprities.add(m)
+    mobs.add(m)
+score = 0
