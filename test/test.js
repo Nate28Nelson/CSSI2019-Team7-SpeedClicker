bb488be3ced2a47a8b90fea95b568f1da6c4814f
Nathan Nelson <nelso185@csusm.edu>
2019-08-06 15:36:50 -0700

    day 2 done

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const target = document.querySelector("#target")
+target.addEventListener("click", function(){
+  target.remove();
+
+  let y = Math.floor((Math.random() * 100))
+  let x = Math.floor((Math.random() * 100))
+
+
+  //TODO: Create function that creates element of same id and randomizes
+  // its location
+});
+
+document.addEventListener("click", (e)=>{
+  console.log((e.x))
+  console.log((e.y))
+});
+  ``
