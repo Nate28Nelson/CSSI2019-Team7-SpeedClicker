c5382a2ce110ca863cbe67c1e1a0fceb782bad90
Nathan Nelson <nelso185@csusm.edu>
2019-08-07 15:14:24 -0700

    Day 3 part 2

--- js/gameindex.js
+++ js/gameindex.js
@@ -1,27 +1,21 @@
+var i = 59;
+const timer = document.querySelector("#timer");
+
 function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  //if(m<0){alert('timer completed')}
-  var s = timeArray[1];
-console.log(timeArray);
-  // var s = checkSecond((timeArray[1] - 1));
-  if(m>0&&s==0){m=m-1; s=59};
-
-  if(s!=0){s=s-1}
-  if(m==0&&s=="00"){alert('game over')}
-
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
+  var countdownTimer = setInterval(function() {
+        console.log(i);
+        timer.innerHTML = i;
+        i = i - 1;
+        if (i <= 0) {
+            clearInterval(countdownTimer);
+            timer.innerHTML = "Game Over";
+        }
+    }, 1000);
 }
 
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec < 0) {sec = "59"};
-  return sec;
+startTimer();
+
+
 
 function changeImg(){
 
