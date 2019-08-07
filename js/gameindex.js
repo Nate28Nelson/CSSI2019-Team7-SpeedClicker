b42e364d92e00526692df722fb1786f903081fde
Nathan Nelson <nelso185@csusm.edu>
2019-08-07 14:38:03 -0700

    changes3

--- js/gameindex.js
+++ js/gameindex.js
@@ -1,49 +1,61 @@
-document.getElementById('timer').innerHTML = 00 + ":" + 10;
-
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec = 0) {sec = "00"};
-  return sec;
-}
-
-
+document.getElementById('timer').innerHTML = 01 + ":" + 00;
 
 function startTimer() {
   var presentTime = document.getElementById('timer').innerHTML;
   var timeArray = presentTime.split(/[:]+/);
   var m = timeArray[0];
-  var s = timeArray[1];
-console.log(timeArray);
-  // var s = checkSecond((timeArray[1] - 1));
-  if(m>0&&s==0){m=m-1; s=59};
-  if(s!=0){s=s-1}
-  if(m==0&&s=="00"){alert('game over')}
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
 
   document.getElementById('timer').innerHTML =
     m + ":" + s;
   setTimeout(startTimer, 1000);
 }
 
-
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
 
 startTimer();
 
-document.getElementById("target").addEventListener("click", function(){
+function changeImg(){
+
+  // document.getElementById("target").addEventListener("click", function(){
   // document.getElementById("target").remove();
-console.log("wow")
-  let y = Math.floor((Math.random() * 1000))
-  let x = Math.floor((Math.random() * 1000))
-  let target = document.getElementById("target")
-  console.log("You did it")
+//1.generate ran #
+    var y = Math.floor(Math.random() * 1000);
+    var x = Math.floor(Math.random() * 1000);
+  //2.add 'px' to ran #(y,x)
+var obj = document.getElementById("target");
+  //height = x + 'px'
+  obj.style.top = y + 'px';
+  obj.style.left = x +'px';
+  //width = y + 'px'
 
-});
+  //3.position: absolute
+  //""style.margintop = height
+  //""style.marginleft = width
 
-document.addEventListener("click", (e)=>{
-  console.log((e.x))
-  console.log((e.y))
-});
+  //4. fingers crossed
+}
 
-top = y
-left = x
+
+
+
+//   let target = document.getElementById("target")
+//   console.log("You did it")
+//
+// });
+//
+// document.addEventListener("click", (e)=>{
+//   console.log((e.x))
+//   console.log((e.y))
+// });
+//
+// top = y
+// left = x
 //TODO: Create function that creates element of same id and randomizes
 // its location
--- test/test.js
+++ test/test.js
@@ -1,17 +1,37 @@
-const target = document.querySelector("#target")
-target.addEventListener("click", function(){
-  target.remove();
+document.getElementById('timer').innerHTML = 01 + ":" + 00;
 
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
+
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
+startTimer();
+
+document.getElementById("target").addEventListener("click", function(){
+  document.getElementById("target").remove();
   let y = Math.floor((Math.random() * 100))
   let x = Math.floor((Math.random() * 100))
-
-
-  //TODO: Create function that creates element of same id and randomizes
-  // its location
+  let target = document.getElementById("target")
 });
 
 document.addEventListener("click", (e)=>{
   console.log((e.x))
   console.log((e.y))
 });
-  
+
+//TODO: Create function that creates element of same id and randomizes
+// its location
